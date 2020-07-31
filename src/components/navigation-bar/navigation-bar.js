8e2838654aaadcaa976ba4489c725e8076c30cab
EdwinTinoco <jaredlotic@gmail.com>
2020-07-31 14:26:53 -0600

    added logo, navbar style and home

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom"
 
+import Logo from "../../../static/assets/images/logo/logo-1.png"
+
 export default class NavigationBar extends Component {
    constructor(props) {
       super(props);
@@ -9,24 +11,39 @@ export default class NavigationBar extends Component {
    render() {
       return (
          <div className="navbar-main-wrapper">
-            <div className="left-column">
-               logo
+            <div className="contact-info">
+               <p>Llamanos para agendar tu cita (801) 123 4567  o agendala online a traves de nuestro sitio web.</p>
             </div>
 
-            <div className="center-column">
-               <div className="links-wrapper">
-                  <div className="link">
-                     <Link to="/">Home</Link>
+            <div className="navbar-info">
+               <div className="left-column">
+                  <img src={Logo} alt="logo" />
+               </div>
+
+               <div className="right-column">
+                  <div className="links-wrapper">
+                     <div className="link">
+                        <Link to="/">Home</Link>
+                     </div>
+
+                     <div className="link">
+                        <Link to="/services">Services</Link>
+                     </div>
+
+                     <div className="link">
+                        <Link to="/services">About</Link>
+                     </div>
+
+                     <div className="link">
+                        <Link to="/services">Contact</Link>
+                     </div>
                   </div>
 
-                  <div className="link">
-                     <Link to="/services">Services</Link>
+                  <div className="login-signup-wrapper">
+                     <p>Login</p>
+                     <p>Sign up</p>
                   </div>
                </div>
-            </div>
-
-            <div className="right-column">
-               login
             </div>
          </div>
       )
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom"
 
 export default class Home extends Component {
    constructor(props) {
@@ -8,7 +9,26 @@ export default class Home extends Component {
    render() {
       return (
          <div className="home-main-wrapper">
-            <h1>Home</h1>
+            <div className="title-info-img">
+               <div className="title">
+                  <p>Tramitaciones y Servicios Diaz</p>
+               </div>
+
+               <div className="info">
+                  <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis
+                     non sem elementum tempor in at urna. Suspendisse auctor libero ut nibh
+                     consequat sed sagittis dolor iaculis. Donec condimentum mauris nec eros
+                     auctor sed vestibulum tellus consequat.
+                  </p>
+               </div>
+
+               <Link to="/auth">
+                  <div className="button">
+                     <p>Agenda tu cita</p>
+                  </div>
+               </Link>
+            </div>
          </div>
       )
    }
